12636357b1c5d95b055c82540a8a367202d148a9
patwari <rahulpatwari233@gmail.com>
2024-06-05 17:41:37 +0530

    Add solution to 1002

---
+++ Leetcode.Test/1002_E_Tests.cs
@@ -0,0 +1,46 @@
+namespace L1002;
+
+public class Tests
+{
+    Solution solution = new();
+
+    [Fact]
+    public void BasicSanityTests()
+    {
+        MainTest(new string[] { "bella", "label", "roller" }, new string[] { "e", "l", "l" }.ToList());
+        MainTest(new string[] { "cool", "lock", "cook" }, new string[] { "c", "o" }.ToList());
+    }
+
+    [Fact]
+    public void EdgeCaseTests()
+    {
+        // Single word - all characters should be returned
+        MainTest(new string[] { "single" }, new string[] { "s", "i", "n", "g", "l", "e" }.ToList());
+
+        // No common characters
+        MainTest(new string[] { "abc", "def", "ghi" }, new List<string>());
+
+        // Identical words
+        MainTest(new string[] { "repeat", "repeat", "repeat" }, new string[] { "r", "e", "p", "e", "a", "t" }.ToList());
+
+        // Empty input
+        MainTest(new string[] { }, new List<string>());
+    }
+
+    [Fact]
+    public void LongStringTests()
+    {
+        // Long strings with many characters
+        string longString1 = new string('a', 1000) + new string('b', 1000);
+        string longString2 = new string('a', 1000) + new string('c', 1000);
+        string longString3 = new string('a', 1000) + new string('d', 1000);
+        string[] correct = new string[1000];
+        for (int i = 0; i < 1000; ++i) correct[i] = "a";
+        MainTest(new string[] { longString1, longString2, longString3 }, correct);
+    }
+
+    private void MainTest(string[] words, IList<string> correct)
+    {
+        Assert.True(EqualUtil.IsEqualUnordered(solution.CommonChars(words), correct));
+    }
+}
---
+++ Leetcode/1002_E_FindCommonCharacters.cs
@@ -0,0 +1,46 @@
+namespace L1002;
+
+/// <summary>
+/// https://leetcode.com/problems/find-common-characters/description/
+/// Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). 
+/// You may return the answer in any order.
+/// <br/><br/>
+/// 
+/// Approach: Frequency Map
+/// Make a central common char map.
+/// For each word, make a frequency map. Then update the central char map.
+/// </summary>
+public class Solution
+{
+    public IList<string> CommonChars(string[] words)
+    {
+        int[] central = new int[26];
+
+        for (int i = 0; i < words.Length; ++i)
+        {
+            int[] freq = new int[26];
+            foreach (char c in words[i])
+            {
+                freq[c - 'a']++;
+            }
+            if (i == 0)
+                central = freq;
+            else
+            {
+                for (int j = 0; j < 26; ++j)
+                    central[j] = Math.Min(central[j], freq[j]);
+            }
+        }
+
+        List<string> output = new();
+        for (int i = 0; i < 26; ++i)
+        {
+            while (central[i] > 0)
+            {
+                output.Add((char)(i + 'a') + "");
+                central[i]--;
+            }
+        }
+        return output;
+    }
+}
